d754628e268f2af684ca5424d396a02bb88ef88b
allanfpcruz <137228336+allanfpcruz@users.noreply.github.com>
2024-04-21 11:07:15 -0300

    término das funcionalidades
    OBS: o projeto apresenta um erro na função de editar, ela não esta alterando a table row referida no event 'click'

--- scrip.js
+++ scrip.js
@@ -21,14 +21,17 @@ function setValues() {
 
   const tableDataName = document.createElement('td')
   tableDataName.innerHTML = values[0]
+  tableDataName.setAttribute('class', 'table-data')
   tableRow.appendChild(tableDataName)
 
   const tableDataFunction = document.createElement('td')
   tableDataFunction.innerHTML = values[1]
+  tableDataFunction.setAttribute('class', 'table-data')
   tableRow.appendChild(tableDataFunction)
 
   const tableDataSalario = document.createElement('td')
   tableDataSalario.innerHTML = values[2]
+  tableDataSalario.setAttribute('class', 'table-data')
   tableRow.appendChild(tableDataSalario)
 
   const tableEditButton = document.createElement('td')
@@ -72,9 +75,28 @@ function getValues() {
   return valuesList
 }
 
-function editDatas(targetEl) {
-  let targetRow = targetEl.closest('tr')
-  return targetRow
+function editDatas(tableDatas) {
+  confirmButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    if(confirmButton.classList.contains('editing')) {
+      let values = getValues()
+      let i = 0
+      console.log(tableDatas)
+      tableDatas.forEach(tableData => {
+        tableData.innerHTML = values[i]
+        i++
+      });
+      toggleInsertArea()
+      formArea.style.opacity = 1
+      resetInputs()
+      confirmButton.classList.remove('editing')
+    }
+  })
+}
+
+//exclui
+function excluir(tableRow) {
+  tableRow.remove()
 }
 
 //eventos
@@ -85,29 +107,35 @@ insertButton.addEventListener('click', (e) => {
 })
 
 confirmButton.addEventListener('click', (e) => {
-  e.preventDefault()
-  toggleInsertArea()
-  resetInputs()
-  if(confirmButton.classList.contains('editing')) {
-    editDatas()
-    alert('ok')
-  } else {
-    alert('ok')
+  if(!confirmButton.classList.contains('editing')) {
+    e.preventDefault()
+    toggleInsertArea()
+    formArea.style.opacity = 1
     setValues()
+    resetInputs()
   }
 })
 
 document.addEventListener('click', (e) => {
   let targetEl = e.target
+  let tableRow = targetEl.closest('tr')
   //editar
   if(targetEl.classList.contains('fa-pen-to-square')) {
-    confirmButton.classList.add('editing')
-    toggleInsertArea()
-    editDatas(targetEl)
+    if(!confirmButton.classList.contains('editing')) {
+      confirmButton.classList.add('editing')
+      let tableDatas = targetEl.closest('tr').querySelectorAll('.table-data')
+      console.log(tableDatas)
+      editDatas(tableDatas)
+      tableDatas = []
+      toggleInsertArea()
+      formArea.style.opacity = .5
+    }
   }
 
   //excluir
   if(targetEl.classList.contains('fa-trash')) {
-
+    excluir(tableRow)
   }
 })
+
+//ERRO, o parâmetro enviado para a função editDatas não é o mesmo recebido, assim, a função altera os valores enviados em outra tr, sem ser a que foi selecionada pelo event 'click'
